250ea8f84f7178d4be964f2f497929915f9585e0
max103520 <maxtsai.0214@gmail.com>
2023-05-20 14:44:14 +0800

    final version

--- products.py
+++ products.py
@@ -1,40 +1,52 @@
 import os
-products = []
-if os.path.isfile('products.csv'):
-    print('Yes')
-    with open('products.csv', 'r', encoding='utf-8') as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue
-		name, price=line.strip().split(',')
-		products.append([name, price])
+
+#讀取檔案
+def read_file(filename):
+    products = []
+    with open(filename, 'r', encoding='utf-8') as f:
+        for line in f:
+            if '商品,價格' in line:
+                continue
+            name, price = line.strip().split(',')
+            products.append([name, price])
+    return products
+
+#讓使用都輸入
+def user_input(products):
+    while True:
+        name = input('請輸入商品名稱:')
+        if name == 'q':
+            break
+        price = input('請輸入商品價格:')
+        price = int(price)
+        products.append([name, price]) #products.append([name, price])
     print(products)
-else:
-	print('No')
+    return(products)
+
+#印出購買記錄
+def print_product(products):
+    for p in products:
+        print(p)
+        print(p[0], '的價格是', p[1])
+
+#寫入檔案
+def write_file(filename, products):
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write('商品,價格\n')
+        for p in products:
+            f.write(p[0] + ','+ str(p[1]) + '\n')        
 
 
+def main():
+    filename = 'products.csv'
+    if os.path.isfile(filename):
+        print('Yes')
+        products = read_file(filename)
+    else:
+        print('No')    
+    products = read_file('products.csv')
+    products = user_input(products)
+    print_product(products)
+    write_file('products.csv', products)
 
-
-
-
-products = []
-while True:
-    name = input('請輸入商品名稱:')
-    if name == 'q':
-        break
-    price = input('請輸入商品價格:')
-    price = int(price)
-    p = []
-    p.append(name)
-    p.append(price)
-    products.append(p) #products.append([name, price])
-print(products)
-
-for p in products:
-	print(p)
-	print(p[0], '的價格是', p[1])
-
-with open('products.csv', 'w', encoding='utf-8') as f:
-	f.write('商品,價格\n')
-	for p in products:
-		f.write(p[0] + ','+ str(p[1]) + '\n')
+main()
